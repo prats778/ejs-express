e5968c69ceae83da0c67079de99beff7eb30c0db
pratyush swain <pratsy17@gmail.com>
2022-01-22 20:46:28 +0530

    setting port env

--- app.js
+++ app.js
@@ -114,6 +114,6 @@ app.get('/:userQuery',(req,res)=>{
 });
 
 
-
-
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
